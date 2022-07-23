a5242ac80f90be7432656727c7ad5d38ae6f421f
Roman Lizunov <rlizunoff@gmail.com>
2022-07-23 15:39:50 +0300

    not work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-import * as basicLightbox from './basicLightbox.min.js'
-const imgEl= document.querySelector('.gallery');
+const containerImg= document.querySelector('.gallery');
 
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `)
+let div, img, a;
 
-// instance.show()
+galleryItems.forEach(({ preview, original, description }) => {
+    // создание элемента
+  div = document.createElement("div");
+  a = document.createElement("a");
+  img = document.createElement("img");
+
+  // добавление классов
+  div.classList.add("gallery__item");
+  img.classList.add("gallery__image");
+
+  // установка атрибутов и текста
+    a.href = preview;
+    img.src = preview;
+    img.dataset.source = original;
+    img.alt = description;
+
+  // внутрь элемента a добавить элемент img
+  a.appendChild(img);
+  // внутрь элемента div добавить элемент a
+  div.appendChild(a);
+  // внутрь элемента body добавить элемент div
+  containerImg.appendChild(div)
+});
+
+const handleClick = (e) => {
+    e.preventDefault();
+    const imgSelected = e.target.classList.contains('gallery__image');
+
+if (!imgSelected) {
+    return;
+} 
+//   if (e.nodeName !== 'BUTTON') {
+//     return;
+//   }
+    const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" width="800" height="600">`);
+    // console.log(e.target.dataset.source);
+    instance.show();
+};
+
+containerImg.addEventListener('click', handleClick);
 
 
-console.log(galleryItems);
+// console.log(galleryItems);
