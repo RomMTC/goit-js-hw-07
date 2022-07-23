1ca40bdb191e627743ccb364136db68e7bffdee7
Roman Lizunov <rlizunoff@gmail.com>
2022-07-23 13:46:20 +0300

    add min js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-import * as basicLightbox from 'basiclightbox'
+import * as basicLightbox from './basicLightbox.min.js'
+const imgEl= document.querySelector('.gallery');
 
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`)
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
 
-instance.show()
+// instance.show()
+
+
 console.log(galleryItems);
